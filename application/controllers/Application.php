c79665620dc51115fa3abb08097c13da697d2945
Laurensius D.S <burglar.tea@gmail.com>
2016-03-25 01:07:52 +0700

    Add Application Class to Application Dir

---
+++ application/controllers/Application.php
@@ -0,0 +1,25 @@
+<?php
+/*
+ * Developed by : Laurensius Dede Suhardiman (Kuningan Iot Dev)
+ * 
+ */
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Application extends CI_Controller {
+    
+    public function __construct() {
+        parent::__construct();
+    }
+    
+    public function index(){
+        $this->load->view('application/landingpage');
+    }
+    
+    public function monitoring(){
+        $this->load->view('monitoring');
+    }
+}
+
+/*
+ *End of controller/Application.php 
+ */
